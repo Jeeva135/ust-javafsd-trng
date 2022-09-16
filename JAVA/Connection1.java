e54fea6167f457b4ad22c170e3469108bcf74e18
Jeeva135 <112387639+Jeeva135@users.noreply.github.com>
2022-09-16 17:54:17 +0530

    Add files via upload

---
+++ JAVA/Connection1.java
@@ -0,0 +1,27 @@
+package week3day4;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Connection1 {
+	public static void main(String args[]) {
+		try {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/company","root","root");
+		Statement s=con.createStatement();
+		ResultSet rs=s.executeQuery("Select * from Employee");
+		while (rs.next()) {
+			System.out.print(rs.getInt(1)+" ");
+			System.out.print(rs.getInt(2)+" ");
+			System.out.println(rs.getString(3)+" ");
+		}
+		con.close();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		}
+
+}
