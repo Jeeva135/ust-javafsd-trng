0708ec77ef4e4151a0de6b9bf8537f45a2635e6c
Jeeva135 <112387639+Jeeva135@users.noreply.github.com>
2022-09-28 18:00:50 +0530

    Update array2.js

--- JAVASCRIPT/array2.js
+++ JAVASCRIPT/array2.js
@@ -1,4 +1,4 @@
-var rainbow= new Array["Violet","Indigo","Blue","Green","Yellow","Orange","Red"];
+var rainbow= new Array("Violet","Indigo","Blue","Green","Yellow","Orange","Red");
 function show(rainbow){
     for(var i=0;i<rainbow.length;i++){
         console.log(rainbow[i]);
