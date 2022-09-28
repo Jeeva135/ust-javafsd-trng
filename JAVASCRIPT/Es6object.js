b06e522311b048dd51b491ce76bf20f022dd8e65
Jeeva135 <112387639+Jeeva135@users.noreply.github.com>
2022-09-28 17:55:29 +0530

    Add files via upload

---
+++ JAVASCRIPT/Es6object.js
@@ -0,0 +1,5 @@
+var student={
+    name:"Jeeva",
+     age:22
+}
+console.log(student);
---
+++ JAVASCRIPT/sample2.js
@@ -0,0 +1,5 @@
+var num1=30;
+var num2=20;
+var res=num1>num2?
+"Yes 30 is greater than 20 ":"Not It's not";
+console.log(res);
