2d9d307ea314b5c0b20147a9ea68ae2b8eec0188
ustjavafsdb219 <235767@ust.com>
2022-10-31 17:51:21 +0530

    done

--- JAVA/Access_modifier.java
+++ JAVA/Access_modifier.java
@@ -1,20 +1,38 @@
-package com.ust;
+package secondset;
 
+/**
+ * The type Access modifier.
+ */
 public class Access_modifier {
+	/**
+	 * The X.
+	 */
 	static int x=1;
 	private int y=3;
+
+	/**
+	 * Method 1.
+	 *
+	 * @param x the x
+	 */
 	public void method1(int x)
 	{
 		Access_modifier t=new Access_modifier();
 		this.x=2;
 		y=4;
 		
-		System.out.println("Test.x:"+Access_modifier.x);
+		System.out.println("Testg.x:"+Access_modifier.x);
 		System.out.println("t.x"+t.x);
 		System.out.println("t.y"+t.y);
 		System.out.println("y"+y);
 		
 	}
+
+	/**
+	 * Main.
+	 *
+	 * @param args the args
+	 */
 	public static void main(String args[]){
 		Access_modifier t =new Access_modifier();
 		t.method1(5);
